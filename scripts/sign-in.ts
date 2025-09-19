ee2a553d63e5a02eae5359734c19b755da2aa01e
CaioHVectorA <caihebatista@gmail.com>
2025-09-19 02:43:49 -0300

    data

--- scripts/sign-in.ts
+++ scripts/sign-in.ts
@@ -2,6 +2,8 @@ import type { Page } from "puppeteer";
 
 export async function signIn(page: Page) {
     // Wait for username input and type
+    await page.goto('https://app.uff.br/graduacao/quadrodehorarios/sessions/new', { waitUntil: 'networkidle2' });
+    if (!page.url().includes('sessions')) return console.log('Already signed in, no need to log in again.');
     await page.waitForSelector('#username', { timeout: 10000 });
     await Bun.sleep(500);
     await page.type('#username', process.env.Q_USERNAME || '');
